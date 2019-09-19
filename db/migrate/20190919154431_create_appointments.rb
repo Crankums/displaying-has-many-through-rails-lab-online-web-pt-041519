8896dbf9af55b7da5a4c727fae3d7adfdd1dd69b
Craig Donavin <crdonavin@gmail.com>
2019-09-19 16:21:25 +0000

    Automatically backed up by Learn

--- db/migrate/20190919154431_create_appointments.rb
+++ db/migrate/20190919154431_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.string :doctor
-      t.string :patient
+      t.string :doctor_id
+      t.string :patient_id
       t.timestamps
     end
   end
